998541f6714e8d63a8549f429fb76e60534a02d3
Gustavo Barbosa <gusbdev@gmail.com>
2020-08-19 11:28:52 -0300

    Updates

---
+++ precache-manifest.f3aed42277f898f6b11078b6a03472ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c224de89f769379e7b9789f2c235c65f",
+    "url": "/react-converter/index.html"
+  },
+  {
+    "revision": "30723e36bde6543fe839",
+    "url": "/react-converter/static/css/main.5b635192.chunk.css"
+  },
+  {
+    "revision": "63f3ccbb72950f11690c",
+    "url": "/react-converter/static/js/2.7f703daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-converter/static/js/2.7f703daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30723e36bde6543fe839",
+    "url": "/react-converter/static/js/main.677b01ab.chunk.js"
+  },
+  {
+    "revision": "d75ad692dcee9ac4b247",
+    "url": "/react-converter/static/js/runtime-main.9e2cb632.js"
+  }
+]);
